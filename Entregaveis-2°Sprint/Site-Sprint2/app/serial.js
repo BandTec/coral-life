2022653f8df4b3d8148e135d0ba0231e1de26f7f
rodriguesvinicius <vinicius.asilva@bandtec.com.br>
2019-10-21 12:50:33 -0300

    Comentando os codigos

--- Entregaveis-2°Sprint/Site-Sprint2/app/serial.js
+++ Entregaveis-2°Sprint/Site-Sprint2/app/serial.js
@@ -1,47 +1,62 @@
 const SerialPort = require('serialport');
+// estamos atibuindo a variavel Serial port todas as funções do serial port que vem do ode
 const Readline = SerialPort.parsers.Readline;
-
+// Read line recebe um função do serial port para ler a linha
 class ArduinoDataRead {
-
+    // estamos criando nossa classe chamada arduinoDataRead para setar nossas funções e devolve-las
     constructor() {
+        // criando um vetor para ser preenchido pelos dados do arduino
         this.listData = [];
     }
 
     get List() {
+        // estamos retornando o vetorTemperatura preenchido
         return this.listData;
     }
 
-    SetConnection() {
 
+    //GET RETORNAMOS ALGO E SET SETAMOS ALGO
+    // iremos localizar o arduino caso o encontramos iremos preencher o vetor com os dados que  vem do arduino
+    SetConnection() {
+        // função para localizar o arduino
         SerialPort.list().then(listSerialDevices => {
 
             let listArduinoSerial = listSerialDevices.filter(serialDevice => {
                 return serialDevice.vendorId == 2341 && serialDevice.productId == 43;
             });
-
+            // se caso ele não achar apresentará o if abaixo
             if (listArduinoSerial.length != 1) {
                 throw new Error("The Arduino was not connected or has many boards connceted");
             }
 
-            console.log("Arduino encontrado na porta %s", listArduinoSerial[0].comName);
+            // caso de certo apresentará o codigo abaixo 
+            console.log("Arduino found in the com %s", listArduinoSerial[0].comName);
 
             return listArduinoSerial[0].comName;
+
         }).then(arduinoCom => {
-
+            // depois de encontrado iremos definir os bits de comunicação com o arduino
             let arduino = new SerialPort(arduinoCom, { baudRate: 9600 });
-
+            // estamos atribuindo a variavel parser tudo que o arduino leu na linha
             const parser = new Readline();
             arduino.pipe(parser);
 
+            // parser.on ele ira preencher o veto de acordo com a linha lida 
             parser.on('data', (data) => {
                 this.listData.push(parseFloat(data));
             });
+
 
         }).catch(error => console.log(error));
     }
 }
 
+
+// aqui estamos instanciando a nossa classe ArduinoDataRead();
 const serial = new ArduinoDataRead();
+// aqui estamos chamando a nossa função setConnection();
 serial.SetConnection();
 
+//estamos exportando nossa classe ArduinoDataRead para qualquer arquivo no nosso projeto
+// e o mesmo esta passando como parametro nosso vetor ja preenchido.
 module.exports.ArduinoData = { List: serial.List }
--- Entregaveis-2°Sprint/Site-Sprint2/js/dashboard.js
+++ Entregaveis-2°Sprint/Site-Sprint2/js/dashboard.js
@@ -1,29 +1,43 @@
 function grafico() {
+    // criando uma variavel context que ira receber o elemento chart
     var context = document.getElementById("chart").getContext("2d");
     context.canvas.width = 1000;
+    // setando uma largura para o grafico
     context.canvas.height = 300;
-    var contador = 0;
+    // setando uma altura para o grafico
+    ///    var contador = 0;
+
     //var years = [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050];
     //var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // nossa variavel de configuração do grafico
     var configuration = {
         type: 'line',
+        // grafico do tipo de linha
+        // data ira fazer nossa estrutura do grafico
         data: {
             // labels: years,
             datasets: [{
+                //nossa legenda do grafico
                 label: "Temperatura",
                 type: 'line',
+                //setando um cor na borda da linha
                 borderColor: "#3e95cd",
+                // o tamanho da borda
                 borderWidth: 3,
+                // fill false nos setamos sem fundo o nosso grafico
                 fill: false,
+                // a cor da nossa linha 
                 backgroundColor: [
                     'rgba(47,54,64, 0.2)'
                 ],
             }],
 
         },
-
+        // opções que ira conter no nosso grafico
         options: {
+            // é index da linha ou seja criará um ponto onde foi armazenado os dados no grafico
             tooltips: {
+                // do tipo ponto
                 mode: 'point'
             },
             scales: {
@@ -52,6 +66,7 @@ function grafico() {
         }
     };
 
+    // criando uma variavel chart para utilizarmos futuramente
     var chart = new Chart(context, configuration);
 
     //Função para obter os dados de temperatura do server
@@ -62,39 +77,47 @@ function grafico() {
     this.lastIndexTemp = 0;
     this.time = 0;
 
+    // criando uma função para preencher nosso grafico
     function get_data() {
-
+        // estamos criando uma varivel para acessar nosso servidor local
         var http = new XMLHttpRequest();
+        // estamos pegando todos os dados que esta vindo do servidor
         http.open('GET', 'http://localhost:3000/api', false);
         http.send(null);
 
+        // estamos atribuindo os dados lidos do servidor a variavel obj que recebe no Json
         var obj = JSON.parse(http.responseText);
 
+        //validando se a data esta vazia
         if (obj.data.length == 0) {
             return;
         }
 
         var _lastIndexTemp = this.lastIndexTemp;
+        // pegar o ultimo index lido
         this.lastIndexTemp = obj.data.length;
         listTemp = obj.data.slice(_lastIndexTemp);
 
+        // estamos criando um laço de repetição que ira preencher nosso grafico com os dados vindo do Json
         listTemp.forEach(data => {
-            //Máximo de 60 itens exibidos no gráfico
+
             if (chart.data.labels.length == 10 && chart.data.datasets[0].data.length == 10) {
                 chart.data.labels.shift();
                 chart.data.datasets[0].data.shift();
             }
-
+            // preechendo nosso grafico com os dados
             chart.data.labels.push(this.time++);
             chart.data.datasets[0].data.push(parseFloat(data));
             chart.update();
         });
-
+        // apresentando a media
         document.getElementById('average').textContent = `Média de temperatura: ${obj.average}`
     }
 
+    //chamando a função getdata() que contém todo nosso
     get_data();
 
+    // o delay que sera para chamar no metodo getdata()
     setInterval(() => {
         get_data();
     }, 1000);
