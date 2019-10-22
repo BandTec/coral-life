43850fec3e8ab25cf63746f23310d3690117d56d
rodriguesvinicius <vinicius.asilva@bandtec.com.br>
2019-10-22 14:48:28 -0300

    Atualizando o codigo do node

--- Entregaveis-2°Sprint/Site-Sprint2/app/controller.js
+++ Entregaveis-2°Sprint/Site-Sprint2/app/controller.js
@@ -4,23 +4,16 @@ const router = express.Router();
 
 
 router.get('/', (request, response, next) => {
-    // soma e média do vetor Temperatura
+
     let sum = ArduinoData.List.reduce((a, b) => a + b, 0);
-    //Média
     let average = (sum / ArduinoData.List.length).toFixed(2);
 
-    // aqui estamos criando o nosso objeto Json
     response.json({
-        // data1 estamos passando o vetor de temperatura
         data: ArduinoData.List,
-        // total1 estamos passando o tamanho total do vetor
         total: ArduinoData.List.length,
-        // avarage estamos passando a média mas antes estamos verificando se a média e vazia
-        average: isNaN(average) ? 0 : average,
-
+        average: isNaN(average) ? 0 : average
     });
 
 });
 
-// exportando nosssa rota contendo todos os dados necessarios
 module.exports = router;
